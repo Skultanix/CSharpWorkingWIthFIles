240f779d788f6f4b718fa426e2e6da47beae1440
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2023-01-11 16:49:04 -0700

    Exercise one completed.

--- WorkingWIthFIles/Program.cs
+++ WorkingWIthFIles/Program.cs
@@ -4,9 +4,18 @@
     {
         static void Main(string[] args)
         {
+            var path = @"C:\Users\cajar\Desktop\UdemyProjects\CSharpFundamentals\WorkingWithFiles\Tomorrow&tommorow&tomorrow.txt";
+            var allChars = File.ReadAllText(path);
+            var words = new List<string>(allChars.Split(new char[] { },StringSplitOptions.RemoveEmptyEntries));
+
             void ExerciseOne()
             {
-
+                Console.WriteLine(string.Format("The number of words in the text file is {0} words.", words.Count()));
+                
+                foreach(var word in words)
+                {
+                    Console.WriteLine(word);
+                }
             }
             void ExerciseTwo()
             {
